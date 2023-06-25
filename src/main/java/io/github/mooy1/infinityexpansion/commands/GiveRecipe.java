90d1a17fd25e96c624c4aee3d46f9096c0c4c240
MAGd_ <109735203+MAG10d@users.noreply.github.com>
2023-06-25 16:49:42 +0800

    Update GiveRecipe.java

--- src/main/java/io/github/mooy1/infinityexpansion/commands/GiveRecipe.java
+++ src/main/java/io/github/mooy1/infinityexpansion/commands/GiveRecipe.java
@@ -19,13 +19,13 @@ import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 public final class GiveRecipe extends SubCommand {
 
     public GiveRecipe() {
-        super("giverecipe", "给予一个 Slimefun 配方中的所有物品", "infinityexpansion.giverecipe");
+        super("giverecipe", "給予該物品在Slimefun合成表中的所有物品", "infinityexpansion.giverecipe");
     }
 
     @Override
     protected void execute(@Nonnull CommandSender sender, @Nonnull String[] args) {
         if (!(sender instanceof Player)) {
-            sender.sendMessage("只有玩家能使用该指令!");
+            sender.sendMessage("只有玩家可以使用該指令!");
             return;
         }
 
@@ -37,11 +37,11 @@ public final class GiveRecipe extends SubCommand {
         SlimefunItem sfItem = SlimefunItem.getById(args[0].toUpperCase());
 
         if (sfItem == null || sfItem instanceof MultiBlockMachine || sfItem.getRecipeType() == RecipeType.GEO_MINER) {
-            sender.sendMessage(ChatColor.RED + "无效的 Slimefun 物品!");
+            sender.sendMessage(ChatColor.RED + "未知 Slimefun 物品!");
             return;
         }
 
-        sender.sendMessage(ChatColor.GREEN + "已给予 " + sfItem.getItemName() + " 的配方");
+        sender.sendMessage(ChatColor.GREEN + "給予配方 " + sfItem.getItemName());
 
         Player p = (Player) sender;
 
